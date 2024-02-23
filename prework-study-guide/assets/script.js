4395427edaab84d66db8aba37b706f17103e556b
storkel <sstorkels@netzero.net>
2024-02-23 10:50:33 -0600

    <cleaned up JS>

--- prework-study-guide/assets/script.js
+++ prework-study-guide/assets/script.js
@@ -11,19 +11,6 @@ function listTopics() {
   
 }
 
-
-
-
-
-/*for (let x = 0; x < topics.length; x++) {
- console.log(topics[x]);
-}
-*/
-
- 
-  
-/*const topic = "HTML";*/
-
 function selectTopic() {
 
 if (randomTopic === 'HTML') {
@@ -43,16 +30,6 @@ if (randomTopic === 'HTML') {
 } else {
   console.log('Please try again!');
 }
-
-/*
-const shapes = ["triangle", "square", "pentagon", "circle"];
-
-for(let x = 0; x < shapes.length; x++)
- {
- console.log(shapes[x]);
-}
-
-*/
 
 }
 console.log('Here are the topics we learned through Prework:');
